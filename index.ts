9841b9d23d8209a72e79978e8f1b5f53c029e2e4
Bnaya Peretz <bnaya@brandtotal.com>
2018-02-17 20:06:36 +0200

    commnets

--- index.ts
+++ index.ts
@@ -5,7 +5,9 @@ console.log(retry);
 console.log(retry.createTimeout);
 
 console.log(retryAsync);
+// this will log undefiend
 console.log(retryAsync.retry);
 
 retry.createTimeout(1, {})
+// this will fail
 retryAsync.retry(() => {}, {});
